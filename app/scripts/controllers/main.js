aaa87dcda26df74d592212792563dc6af2e680a1
Hristo Chakarov <ickata@ickata-vpn.bg.nc>
2014-07-23 21:48:44 +0300

    Adding new TODO item

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('todoAppApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.todos = [
-      'Get up',
-      'Go to the gym',
-      'Reach office'
-    ];
-  });
+   .controller('MainCtrl', function ($scope) {
+      $scope.todos = [
+         'Get up',
+         'Go to the gym',
+         'Reach office'
+      ];
+      $scope.addTodo = function () {
+         $scope.todos.push( $scope.todo );
+         $scope.todo = '';
+      };
+   });
